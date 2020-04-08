7ccc0b3a214a11d5c1f72ecd83a0280191d99c6f
Yarkin13 <stas20072008@yandex.ru>
2020-04-08 19:13:16 +0400

    feat: add functional menu

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const BURGER_BTN = document.getElementById('burgerMenu');
-BURGER_BTN.addEventListener('click', () => {
-    if(document.querySelector('span').classList.contains('burgerMenu_active')) {
-        document.querySelector('span').classList.remove('burgerMenu_active')
-    } else document.querySelector('span').classList.add('burgerMenu_active');
+const BURGER_BTN = document.getElementById('burger-checkbox');
+BURGER_BTN.addEventListener('click', (event) => {
+    let target = event.target;
+    console.log(event.target)
+    if(target.tagName === 'SPAN' || target.tagName === 'INPUT') {
+        if(document.querySelector('span').classList.contains('burgerMenu_active')) {
+            document.querySelector('span').classList.remove('burgerMenu_active')
+        } else document.querySelector('span').classList.add('burgerMenu_active');
+    }
+    if(document.querySelector('.menu').classList.contains('showMenu')) {
+        document.querySelector('.menu').classList.remove('showMenu')
+    } else document.querySelector('.menu').classList.add('showMenu')
 })
 
