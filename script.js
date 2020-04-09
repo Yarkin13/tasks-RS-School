7b9e15cabe5923e3d3942ba7a52c5a11e190f8fb
Yarkin13 <stas20072008@yandex.ru>
2020-04-09 14:46:33 +0400

    feat: add structure main page

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const BURGER_BTN = document.getElementById('burger-checkbox');
 BURGER_BTN.addEventListener('click', (event) => {
     let target = event.target;
-    console.log(event.target)
     if(target.tagName === 'SPAN' || target.tagName === 'INPUT') {
-        if(document.querySelector('span').classList.contains('burgerMenu_active')) {
-            document.querySelector('span').classList.remove('burgerMenu_active')
-        } else document.querySelector('span').classList.add('burgerMenu_active');
+        if(document.querySelector('span').classList.contains('burger-menu_active')) {
+            document.querySelector('span').classList.remove('burger-menu_active')
+        } else document.querySelector('span').classList.add('burger-menu_active');
     }
-    if(document.querySelector('.menu').classList.contains('showMenu')) {
-        document.querySelector('.menu').classList.remove('showMenu')
-    } else document.querySelector('.menu').classList.add('showMenu')
+    if(document.querySelector('.burger-menu__menu').classList.contains('show-menu')) {
+        document.querySelector('.burger-menu__menu').classList.remove('show-menu')
+    } else document.querySelector('.burger-menu__menu').classList.add('show-menu')
 })
 
