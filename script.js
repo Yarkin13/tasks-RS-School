334d00fe757862e0fb172ec19ae18774552e2457
Yarkin13 <stas20072008@yandex.ru>
2020-04-09 22:02:14 +0400

    docs: add img and audio feat: add media. add switch button

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const BURGER_BTN = document.getElementById('burger-checkbox');
+const MODE = document.getElementById('mode');
 BURGER_BTN.addEventListener('click', (event) => {
-    let target = event.target;
-    if(target.tagName === 'SPAN' || target.tagName === 'INPUT') {
-        if(document.querySelector('span').classList.contains('burger-menu_active')) {
-            document.querySelector('span').classList.remove('burger-menu_active')
-        } else document.querySelector('span').classList.add('burger-menu_active');
-    }
-    if(document.querySelector('.burger-menu__menu').classList.contains('show-menu')) {
-        document.querySelector('.burger-menu__menu').classList.remove('show-menu')
-    } else document.querySelector('.burger-menu__menu').classList.add('show-menu')
-})
+  const target = event.target;
+  if (target.tagName === 'SPAN' || target.tagName === 'INPUT') {
+    if (document.querySelector('span').classList.contains('burger-menu_active')) {
+      document.querySelector('span').classList.remove('burger-menu_active');
+    } else document.querySelector('span').classList.add('burger-menu_active');
+  }
+  if (document.querySelector('.burger-menu__menu').classList.contains('show-menu')) {
+    document.querySelector('.burger-menu__menu').classList.remove('show-menu');
+  } else document.querySelector('.burger-menu__menu').classList.add('show-menu');
+});
 
+const SWITCH_MODE = document.getElementById('switch-mode');
+SWITCH_MODE.addEventListener('click', (event) => {
+  if (event.target.checked === true) {
+    document.querySelectorAll('.container-card__items').forEach(el => el.classList.add('container-card__items_mode-play'));
+    MODE.innerHTML = 'Mode:  exam';
+    document.querySelector('.burger-menu__menu').classList.add('burger-menu__menu_mode-play')
+    document.querySelector('.switch-mode__for').classList.add('switch-mode__for_mode-play')
+  } else {
+    document.querySelectorAll('.container-card__items').forEach(el => el.classList.remove('container-card__items_mode-play'));
+    MODE.innerHTML = 'Mode: train'
+    document.querySelector('.burger-menu__menu').classList.remove('burger-menu__menu_mode-play')
+    document.querySelector('.switch-mode__for').classList.remove('switch-mode__for_mode-play')
+  }
+});
+console.log(object.name)
